acbb25191bc797b48f80a2c71637be99cc03a039
Bryce Tsuyuki <btsuyuki1@gmail.com>
2020-06-09 21:50:46 -0700

    finish about me and contact section

--- js/script.js
+++ js/script.js
@@ -21,27 +21,5 @@ document.addEventListener('DOMContentLoaded', () => {
         });
       });
     }
-
-    // const $resumeDropdown = Array.prototype.slice.call(document.querySelectorAll('.navbar-link'), 0);
-  
-    // // Check if there are any navbar dropdowns
-    // if ($resumeDropdown.length > 0) {
-  
-    //   // Add a click and hover event on each of them
-    //   $resumeDropdown.forEach( el => {
-    //     el.addEventListener('click', () => {
-  
-    //       // Get the target from the "data-target" attribute
-    //       const target = el.dataset.target;
-    //       const $target = document.getElementById(target);
-
-    //       // Toggle the "is-active" class on both the "navbar-burger" and the "navbar-menu"
-    //       el.classList.toggle('is-active');
-    //       $target.classList.toggle('is-active');
-  
-    //     });
-
-    //   });
-    // }
   
   });
